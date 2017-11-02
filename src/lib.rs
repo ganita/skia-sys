fee3aee9c25e71c76086033f698a496486604344
Sreejith Krishnan R <sreejith@ganita.io>
2017-11-02 15:33:13 +0530

    Export ffi

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+mod bindings;
+
+pub use bindings::*;
