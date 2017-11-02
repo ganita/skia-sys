4940eca4177b44a0282c318d49750dfc17c17f4d
Sreejith Krishnan R <sreejith@ganita.io>
2017-11-02 16:00:46 +0530

    API for color

--- src/lib.rs
+++ src/lib.rs
@@ -22,3 +22,23 @@
 mod bindings;
 
 pub use bindings::*;
+
+pub fn sk_color_set_argb(a: u8, r: u8, g: u8, b: u8) -> sk_color_t {
+    (a as u32) << 24 | (r as u32) << 16 | (g as u32) << 8 | (b as u32)
+}
+
+pub fn sk_color_get_a(color: sk_color_t) -> u8 {
+    (color >> 24 & 0xFF) as u8
+}
+
+pub fn sk_color_get_r(color: sk_color_t) -> u8 {
+    (color >> 16 & 0xFF) as u8
+}
+
+pub fn sk_color_get_g(color: sk_color_t) -> u8 {
+    (color >> 8 & 0xFF) as u8
+}
+
+pub fn sk_color_get_b(color: sk_color_t) -> u8 {
+    (color >> 0 & 0xFF) as u8
+}
